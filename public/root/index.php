ede49b82eb6f316837b342924d82c9bfb006ac91
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-11-03 13:20:17 +0100

    hello

--- public/root/index.php
+++ public/root/index.php
@@ -25,7 +25,7 @@ require_once("./user_actions.php");
     </header>
     <main>
         <section class="explorer">
-            <form action="./new_folder.php" method="post" class="new-">
+            <form action="./user_actions.php" method="post" class="new-">
                 <input type="text" name="newFolder" class="explorer__new">
                 <button type="submit" class="new-folder"> New Folder</button>
             </form>
@@ -40,7 +40,7 @@ require_once("./user_actions.php");
             </div>
             <div class="content__folder">
                 <?php
-                $basePath = "C:/XAMPP/htdocs/PhpBasicWorkshop/manage-files/filesystem-explorer/public/root/Files";
+                $basePath = "./Files";
                 $dirContent = scandir($basePath);
                 foreach ($dirContent as $v) {
                     if (is_array($v)) {
--- public/root/user_actions.php
+++ public/root/user_actions.php
@@ -3,16 +3,9 @@
 function newFolder()
 {
     $folderName = $_POST["newFolder"];
-    $path = "C:/XAMPP/htdocs/PhpBasicWorkshop/manage-files/filesystem-explorer/public/root/Files/$folderName";
-
+    $path = "./Files/$folderName";
 
     mkdir($path, 0777, true);
-
-    $content = scandir("C:/XAMPP/htdocs/PhpBasicWorkshop/manage-files/filesystem-explorer/public/root/Files/$folderName");
-
-    print_r($content);
-
-    //Crear los divs que sean
 
     header("Location: ./index.php");
 }
