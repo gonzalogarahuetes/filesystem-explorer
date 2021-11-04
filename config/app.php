93a9c77a74f4bd18d219981c653acee7534f3bd4
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-11-04 10:07:56 +0100

    After merge

--- config/app.php
+++ config/app.php
@@ -1,5 +1,4 @@
 <?php
 /* Help to set up and allow PHP to generate the link to other pages file automatically */
-define ( "BASE_URL", "/filesystem-explorer/" );
-define ( "ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/filesystem-explorer/" );
-?>
+define ( "BASE_URL", "/PhpBasicWorkshop/manage-files/filesystem-explorer/" );
+define ( "ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/PhpBasicWorkshop/manage-files/filesystem-explorer/" );
--- public/root/index.php
+++ public/root/index.php
@@ -8,126 +8,127 @@ require_once("./user_actions.php");
 $title = "Index";
 include(ROOT_PATH . "inc/_head.php");
 ?>
-    <header class="header">
-        <img class='fileIcon-large' src="../../assets/icons/bug.svg">
-        <input type="search" class="header__search" placeholder="Search">
-        <p class="header_welcome">Welcome, <?= $_SESSION["username"] ?></p>
-        <a href="../logout.php" class="log_out">Logout</a>
-    </header>
-    <main class="main">
-        <section class="explorer">
-            <form action="./new_folder.php" method="post" class="new-">
-                <input type="text" name="newFolder" class="explorer__new">
-                <button type="submit" class="new-folder"> New Folder</button>
-            </form>
-            <div class="explorer__folders">
-                <div class="explorer__folders-root">
-                    <img class='fileIcon' src='./Icons/folder.svg'>
-                    <h3>/root</h3>
-                </div>
-                <?php
-                    $basePath = "./Files";
-                    function listFolderFiles($basePath) {
-                        $items = scandir($basePath);
-                        unset($items[array_search('.', $items, true)]);
-                        unset($items[array_search('..', $items, true)]);
-                        // prevent empty ordered elements
-                        if (count($items) < 1)
-                            return;
-                        foreach($items as $item){
-                            $fileExtension = explode(".", $item);
-                            if(is_dir($basePath.'/'.$item))
-                            {
-                                echo "<div class='display_folder-title'><img class='fileIcon' src='./Icons/folder.svg'><p class='folder1__element'><a href='./select-file.php?file=$item' class='link'>$item</a></p></div>";
-                                // listFolderFiles($basePath.'/'.$item);
-                            } else {
-                                echo "<div class='display_folder-title'><img class='fileIcon' src='./Icons/$fileExtension[1].svg'><p class='folder1__element'><a href='./select-file.php?file=$item' class='link'>$item</a></p></div>";
-                            }
-                        }
-                    }
-                    listFolderFiles($basePath);
-                ?>
-            </div>
-        </section>
-        <section class="content">
-            <div class="content__guide">
-                <p class="guide__p guide__p-left">Name</p>
-                <div class="guide__p-right">
-                    <p class="guide__p">Size</p>
-                    <p class="guide__p">Modified</p>
-                    <button class="guide_upload"><img class='fileIcon-small' src="../../assets/icons/cloudup.svg"></i></button>
-                </div>
-            </div>
-            <div class="content__folder">
+<header class="header">
+    <img class='fileIcon-large' src="../../assets/icons/bug.svg">
+    <input type="search" class="header__search" placeholder="Search">
+    <p class="header_welcome">Welcome, <?= $_SESSION["username"] ?></p>
+    <a href="../logout.php" class="log_out">Logout</a>
+</header>
+<main class="main">
+    <section class="explorer">
+        <form action="./new_folder.php" method="post" class="new-">
+            <input type="text" name="newFolder" class="explorer__new">
+            <button type="submit" class="new-folder"> New Folder</button>
+        </form>
+        <div class="explorer__folders">
+            <div class="explorer__folders-root">
                 <img class='fileIcon' src='./Icons/folder.svg'>
-                <p class="content__folder-title">Others</p>
+                <h3>/root</h3>
             </div>
-            <div class="content__list">
-                <?php
-                    $basePath = "./Files";
-                    $dirContent = scandir($basePath);
-                    foreach ($dirContent as $v) {
-                        $fileExtension = explode(".", $v);
-                        $sizeOfFile = get_folder_size($basePath . "/" . $v);
-                        $timeModified = date("F d Y", filemtime($basePath . "/" . $v));
-                        if(!is_file($basePath . "/" . $v)) {
-                            if (!($v == '.')) {
-                                if (!($v == '..')) {
-                                    echo "
+            <?php
+            $basePath = "./Files";
+            function listFolderFiles($basePath)
+            {
+                $items = scandir($basePath);
+                unset($items[array_search('.', $items, true)]);
+                unset($items[array_search('..', $items, true)]);
+                // prevent empty ordered elements
+                if (count($items) < 1)
+                    return;
+                foreach ($items as $item) {
+                    $fileExtension = explode(".", $item);
+                    if (is_dir($basePath . '/' . $item)) {
+                        echo "<div class='display_folder-title'><img class='fileIcon' src='./Icons/folder.svg'><p class='folder1__element'><a href='./select-file.php?file=$item' class='link'>$item</a></p></div>";
+                        // listFolderFiles($basePath.'/'.$item);
+                    } else {
+                        echo "<div class='display_folder-title'><img class='fileIcon' src='./Icons/$fileExtension[1].svg'><p class='folder1__element'><a href='./select-file.php?file=$item' class='link'>$item</a></p></div>";
+                    }
+                }
+            }
+            listFolderFiles($basePath);
+            ?>
+        </div>
+    </section>
+    <section class="content">
+        <div class="content__guide">
+            <p class="guide__p guide__p-left">Name</p>
+            <div class="guide__p-right">
+                <p class="guide__p">Size</p>
+                <p class="guide__p">Modified</p>
+                <button class="guide_upload"><img class='fileIcon-small' src="../../assets/icons/cloudup.svg"></i></button>
+            </div>
+        </div>
+        <div class="content__folder">
+            <img class='fileIcon' src='./Icons/folder.svg'>
+            <p class="content__folder-title">Others</p>
+        </div>
+        <div class="content__list">
+            <?php
+            $basePath = "./Files";
+            $dirContent = scandir($basePath);
+            foreach ($dirContent as $v) {
+                $fileExtension = explode(".", $v);
+                $sizeOfFile = get_folder_size($basePath . "/" . $v);
+                $timeModified = date("F d Y", filemtime($basePath . "/" . $v));
+                if (!is_file($basePath . "/" . $v)) {
+                    if (!($v == '.')) {
+                        if (!($v == '..')) {
+                            echo "
                                         <div class='display_folder'>
                                             <img class='fileIcon' src='./Icons/folder.svg'>
                                             <p class='folder1__element'>$v</p>
                                             <p>$sizeOfFile</p>
                                             <p>$timeModified</p>
                                         </div>";
-                                }
-                            }
-                        } else {
-                            echo "
+                        }
+                    }
+                } else {
+                    echo "
                                     <div class='display_folder'>
                                         <img class='fileIcon' src='./Icons/$fileExtension[1].svg'>
                                         <p class='folder1__element'>$v</p>
                                         <p>$sizeOfFile</p>
                                         <p>$timeModified</p>
                                     </div>";
-                        }
-                    }
+                }
+            }
 
-                    function get_folder_size($folder) {
-                        $total_size = 0;
-                        if(is_file($folder)) $total_size = $total_size + filesize($folder);
-                        if(is_dir($folder)) {
-                            $files = scandir($folder);
-                            foreach($files as $file) {
-                                if($file === '.' or $file === '..') {
-                                    continue;
-                                } else {
-                                   $path = $folder . '/' . $file;
-                                   $total_size = $total_size + filesize($path);
-                                   get_folder_size($path);
-                                }
-                            }
+            function get_folder_size($folder)
+            {
+                $total_size = 0;
+                if (is_file($folder)) $total_size = $total_size + filesize($folder);
+                if (is_dir($folder)) {
+                    $files = scandir($folder);
+                    foreach ($files as $file) {
+                        if ($file === '.' or $file === '..') {
+                            continue;
+                        } else {
+                            $path = $folder . '/' . $file;
+                            $total_size = $total_size + filesize($path);
+                            get_folder_size($path);
                         }
-                        return $total_size;
                     }
-                ?>
-            </div>
-        </section>
-        <section class="details">
-            <div class="details__title">
-                <i></i>
-                <p>Title</p>
-                <button class="details__btn--edit"><img class='fileIcon-medium' src="../../assets/icons/edit.svg"></button>
-                <button class="details__btn--delete"><img class='fileIcon-medium' src="../../assets/icons/delete.svg"></button>
-            </div>
-            <div class="details__content">
-                <p>Type <span>PHP<span></p>
-                <p>Size <span>500Kb<span></p>
-                <p>Modified <span>12/01/2021<span></p>
-                <p>Created <span>1/01/2021<span></p>
-            </div>
-        </section>
-    </main>
+                }
+                return $total_size;
+            }
+            ?>
+        </div>
+    </section>
+    <section class="details">
+        <div class="details__title">
+            <i></i>
+            <p>Title</p>
+            <button class="details__btn--edit"><img class='fileIcon-medium' src="../../assets/icons/edit.svg"></button>
+            <button class="details__btn--delete"><img class='fileIcon-medium' src="../../assets/icons/delete.svg"></button>
+        </div>
+        <div class="details__content">
+            <p>Type <span>PHP<span></p>
+            <p>Size <span>500Kb<span></p>
+            <p>Modified <span>12/01/2021<span></p>
+            <p>Created <span>1/01/2021<span></p>
+        </div>
+    </section>
+</main>
 </body>
 
 </html>
