949790d2146b1adeffe7aa1faaf2682b6d828bc2
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-11-05 14:56:38 +0100

    Working on sign up

--- public/session_control.php
+++ public/session_control.php
@@ -69,3 +69,50 @@ function destroyCookie()
         );
     }
 }
+
+function signup()
+{
+    if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["rep-password"])) {
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $reppass = $_POST["rep-password"];
+    } else {
+        header("Location: ./signup-page.php?error=incomplete");
+    }
+
+    //TODO check if both passwords match
+
+    checkSignUp($username, $password, $reppass);
+
+    $explodePath = explode("public", __DIR__);
+    $usersJsonFile = $explodePath[0] . "assets\data\users.json";
+
+    if (file_exists($usersJsonFile)) {
+        $jsonData = file_get_contents($usersJsonFile);
+
+        $usersData = json_decode($jsonData, true);
+    } else {
+        $usersData = [];
+    }
+
+    $usersData[] = [
+        "username" => $username,
+        "email" => $email,
+        "password" => $password,
+    ];
+
+    $jsonData = json_encode($usersData, JSON_PRETTY_PRINT);
+
+    file_put_contents($usersJsonFile, $jsonData); //this line equals:
+    //fopen($usersJsonFile, "w");
+    //fwrite($usersJsonFile, $jsonData);
+    //fclose($usersJsonFile);
+
+
+    // header("Location: ./");
+}
+
+function checkSignUp($username, $password, $reppass)
+{
+}
--- public/singup-page.php
+++ public/singup-page.php
@@ -3,22 +3,25 @@ require_once("../config/app.php");
 require_once("./session_control.php");
 session_start();
 isset($_SESSION["username"]) ? header("Location: ./root") : "";
-$title = "Landing Page";
+$title = "Sign Up Page";
 include(ROOT_PATH . "inc/_head.php");
 ?>
+
 <div class="darkbg">
     <h1 class="login__title">A FILE MANAGER FOR THE ORGANIZED ONES</h1>
     <h2 class="login__subtitle">Create your own architecture of folders and keep track of all your stuff!</h2>
 
     <div class="login__div">
-        <form method="post" action="./login.php" class="login">
+        <form method="post" action="./signup.php" class="login">
             <input type="text" class="login__input" placeholder="username" name="username">
+            <input type="text" class="login__input" placeholder="email" name="email">
             <input type="password" class="login__input" placeholder="password" name="password">
-            <button type="submit" class="login__btn">Log in!</button>
+            <input type="password" class="login__input" placeholder="repeat password" name="rep-password">
+            <button type="submit" class="login__btn">Sign up!</button>
         </form>
     </div>
-    <h2 class="login__notlogged">Not registered yet? <a class="signup" href="./singup-page.php">Sign up</a> and start!</h2>
     </body>
 </div>
+
 
 </html>
