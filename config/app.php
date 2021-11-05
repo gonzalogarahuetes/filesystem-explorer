78b81f277c5e0bfc4c724f1d765fc825e962215b
SalvaBandicoot <salvabandicoot@gmail.com>
2021-11-05 11:49:14 +0100

    Start search bar funcionability

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 /* Help to set up and allow PHP to generate the link to other pages file automatically */
-define("BASE_URL", "/PhpBasicWorkshop/manage-files/filesystem-explorer/");
-define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/PhpBasicWorkshop/manage-files/filesystem-explorer/");
+define("BASE_URL", "/filesystem-explorer/");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/filesystem-explorer/");
 
 // echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
